f95a0e3e4ddf6c459d5b46aa101729291606fc7b
Haider Ali <lethalhaider@gmail.com>
2021-10-12 16:54:46 +0530

    Added files from local storage

---
+++ index.js
@@ -0,0 +1,50 @@
+let myLeads = [];//This array stores all the leads
+const input = document.getElementById("input");
+const btn = document.getElementById("submit");
+const ulEl = document.getElementById("value");
+let deleteBtn = document.getElementById("delete");
+let saveTabBtn = document.getElementById("saveTab")
+const tabs = [{url:"www.google.com"}];
+
+saveTabBtn.addEventListener("click",function(){
+    chrome.tabs.query({active:true,currentWindow:true},function(tabs){  
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("myLeads",JSON.stringify(myLeads));
+        render(myLeads);
+    })
+})
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+if(leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage;
+    render(myLeads);
+}
+
+btn.addEventListener("click",function(){
+    myLeads.push(input.value);
+    input.value = "";
+    localStorage.setItem("myLeads",JSON.stringify(myLeads));
+    render(myLeads);
+})
+
+function render(leads){
+    let listItems = ""
+    for(let i = 0;i<leads.length;i++){
+    listItems += `
+        <li>
+            <a href = '${leads[i]}' target= '_blank'>
+            ${leads[i]}
+            </a>
+        </li>
+    `
+    }
+    ulEl.innerHTML = listItems;
+}
+
+
+deleteBtn.addEventListener("dblclick",function(){
+    localStorage.clear(); 
+    myLeads = [];
+    render(myLeads);
+})
+
